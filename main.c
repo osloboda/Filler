c67db77340e2a01e461e6f5de2d6d6a8374bed56
Oleh SLOBODANIUK <osloboda@e1r6p14.unit.ua>
2019-02-26 21:57:24 +0200

    ff

--- main.c
+++ main.c
@@ -20,11 +20,38 @@ void				clear_by_color(SDL_Surface *surface, unsigned int color)
 	}
 }
 
+void drawText
+        (SDL_Surface* screen, char* string,
+         int size, int x, int y,
+         int fR, int fG, int fB,
+         int bR, int bG, int bB)
+{
+
+    TTF_Font* font = TTF_OpenFont("arial.ttf", size);
+
+    SDL_Color foregroundColor = { fR, fG, fB, fB};
+    SDL_Color backgroundColor = { bR, bG, bB, bB};
+
+    SDL_Surface* textSurface
+            = TTF_RenderText_Shaded
+                    (font, string, foregroundColor, backgroundColor);
+
+    SDL_Rect textLocation = { x, y, 0, 0 };
+
+    SDL_BlitSurface(textSurface, NULL, screen, &textLocation);
+
+    SDL_FreeSurface(textSurface);
+
+    TTF_CloseFont(font);
+}
+
 int		render2d(SDL_Surface *surface, t_map *map)
 {
 	char 	*line;
 	int 	y;
 	int 	x;
+	int     i;
+	int     j;
 	unsigned int	*pixel;
 
 	y = -1;
@@ -37,16 +64,23 @@ int		render2d(SDL_Surface *surface, t_map *map)
 		while (++x < map->ma_x)
 		{
 			if (line[x + 4] == '.')
-				map->col = 0xFFFFFF;
+				map->col = 0x434560;
 			else if (line[x + 4] == 'O' || line[x + 4] == 'o')
-				map->col = 0xFF0000;
+				map->col = 0xB5EE70;
 			else
-				map->col = 0x0000FF;
-			pixel[((x) * (int) map->k) + (((y + 100) * (int) map->k) * map->win_x + 100)]= map->col;
-		//	pixel[(x * (int) map->k) + ((y * (int) map->k) * (int) map->k * map->win_x) + 1] = map->col;
-		//	pixel[(x * (int) map->k) + ((y * (int) map->k) * (int) map->k * map->win_x) + 2] = map->col;
-		//	pixel[(x * (int) map->k) + ((y * (int) map->k) * (int) map->k * map->win_x) + 3] = map->col;
-		//	pixel[(x * (int) map->k) + ((y * (int) map->k) * (int) map->k * map->win_x) + 1] = map->col;
+				map->col = 0x1A93B0;
+			pixel[((x) * (int) map->k) + ((y * (int) map->k) * map->win_x + 100) + map->win_x * 100] = map->col;
+			i = 0;
+			while (i < map->k - 3)
+            {
+			    j = 0;
+			    while (j < map->k - 3)
+                {
+                    pixel[((x) * (int) map->k + j) + ((y * (int) map->k) * map->win_x + map->win_x * i + 100) + map->win_x * 100] = map->col;
+                    j++;
+                }
+			    i++;
+            }
 		}
 		free(line);
 	}
@@ -59,8 +93,26 @@ int			loop_key_hook(t_map *map, SDL_Surface *surface, SDL_Window *window)
 	char	*tmp;
 	SDL_Event		event;
 	const Uint8		*keystate;
-
-	keystate = SDL_GetKeyboardState(NULL);
+    keystate = SDL_GetKeyboardState(NULL);
+    SDL_Surface *pla1;
+    pla1 = SDL_GetWindowSurface(window);
+  //  SDL_Renderer* Main_Renderer = NULL;
+ //   Main_Renderer = SDL_CreateRenderer(window, -1, 0);
+   // TTF_Init();
+//    TTF_Font* Sans = TTF_OpenFont("arial.ttf", 25);
+//    SDL_Color White = {255, 255, 255, 255};  // this is the color in rgb format, maxing out all would give you the color white, and it will be your text's color
+//    SDL_Surface* surfaceMessage = TTF_RenderText_Solid(Sans, map->p1, White);
+   // TTF_Quit();
+//    SDL_Texture* Message = SDL_CreateTextureFromSurface(Main_Renderer, surfaceMessage); //now you can convert it into a texture
+    SDL_Rect Message_rect; //create a rect
+    Message_rect.x = 0;  //controls the rect's x coordinate
+    Message_rect.y = 0; // controls the rect's y coordinte
+    Message_rect.w = 100; // controls the width of the rect
+    Message_rect.h = 100; // controls the height of the rect
+//
+//    SDL_RenderCopy(Main_Renderer, Message, NULL, &Message_rect);
+//    SDL_RenderPresent(Main_Renderer);
+//    SDL_SetRenderDrawColor(Main_Renderer, 0xFF, 0xFF, 0xFF, 0xFF );
 	while (get_next_line(0, &line))
 	{
 		if (*line == '$')
@@ -79,9 +131,10 @@ int			loop_key_hook(t_map *map, SDL_Surface *surface, SDL_Window *window)
 				map->ma_x = ft_atoi((ft_strchr(ft_strchr(line, ' ') + 1, ' ') + 1));
                 map->k = 25;
                 if (map->ma_y == 15)
-				    map->k = 45;
-                else
+				    map->k = 50;
+                else if (map->ma_y == 100)
                     map->k = 8;
+
 			}
 		}
 		if (!ft_strncmp(line, "Plateau ", 8))
@@ -89,6 +142,8 @@ int			loop_key_hook(t_map *map, SDL_Surface *surface, SDL_Window *window)
 			ft_strdel(&line);
 			get_next_line(0, &line);
 			render2d(surface, map);
+            drawText(pla1, map->p1, 24, 0, 0, 255, 255, 255, 0, 0, 255);
+            SDL_BlitSurface(surface, &Message_rect, pla1, NULL);
 			SDL_UpdateWindowSurface(window);
 			if (SDL_PollEvent(&event))
 			{
@@ -122,7 +177,7 @@ int					main(void)
 	keystate = SDL_GetKeyboardState(NULL);
 
 	is_running = 1;
-	clear_by_color(surface, 0x000000);
+	clear_by_color(surface, 0x362865);
 	SDL_UpdateWindowSurface(window);
 	while (is_running)
 	{
